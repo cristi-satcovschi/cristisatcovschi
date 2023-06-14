aa859b766f2f6f78f200ab395cf521e9bea65e73
cristi <cristisatcovschi123@gmail.com>
2023-06-14 18:58:49 +0300

    update services and contact sections

--- apps/nextjs/src/pages/index.tsx
+++ apps/nextjs/src/pages/index.tsx
@@ -35,7 +35,7 @@ const AppCard = ({ app }) => {
 };
 
 const InfoCard = ({ children }) => {
-  return <div className="bg-slate-50 rounded p-4 shadow">{children}</div>;
+  return <div className="bg-slate-50 rounded p-6 shadow">{children}</div>;
 };
 
 const FeedCard = ({ children }) => {
@@ -130,9 +130,109 @@ const Home: NextPage = () => {
           {/* content -- info */}
           <div className="m-8 w-1/3">
             <div className="flex flex-col space-y-8">
-              <InfoCard>Info</InfoCard>
-              <InfoCard>Servicii</InfoCard>
-              <InfoCard>Contact</InfoCard>
+              <InfoCard>
+                <h2 className="font-semibold">Servicii</h2>
+                <hr className="mb-6 mt-4" />
+
+                <ul className="space-y-2">
+                  <li className="flex flex-row space-x-8">
+                    <img
+                      width="64"
+                      height="64"
+                      src="https://img.icons8.com/stickers/100/design.png"
+                      alt="design"
+                    />
+                    <div className="flex flex-grow items-center">
+                      <span className="text-slate-900 font-medium">Design</span>
+                    </div>
+                  </li>
+                  <li className="flex flex-row space-x-8">
+                    <img
+                      width="64"
+                      height="64"
+                      src="https://img.icons8.com/stickers/100/source-code.png"
+                      alt="design"
+                    />
+                    <div className="flex flex-grow items-center">
+                      <span className="text-slate-900 font-medium">
+                        Dezvoltare
+                      </span>
+                    </div>
+                  </li>
+                </ul>
+              </InfoCard>
+
+              <InfoCard>
+                <h2 className="font-semibold">Contact</h2>
+                <hr className="mb-6 mt-4" />
+
+                <ul className="space-y-2">
+                  <li className="flex flex-row space-x-8">
+                    <img
+                      width="64"
+                      height="64"
+                      src="https://img.icons8.com/stickers/100/email-sign.png"
+                      alt="email-sign"
+                    />
+                    <div className="flex flex-grow items-center">
+                      <a
+                        className="text-blue-600 dark:text-blue-500 break-all font-medium underline hover:no-underline"
+                        href="mailto: cristi.satcovschi@gmail.com?subject = Feedback&body = Message"
+                      >
+                        cristi.satcovschi@gmail.com
+                      </a>
+                    </div>
+                  </li>
+                  <li className="flex flex-row space-x-8">
+                    <img
+                      width="64"
+                      height="64"
+                      src="https://img.icons8.com/stickers/100/facebook-messenger.png"
+                      alt="facebook-messenger"
+                    />
+                    <div className="flex flex-grow items-center">
+                      <a
+                        className="text-blue-600 dark:text-blue-500 break-all font-medium underline hover:no-underline"
+                        href="https://m.me/cristi.satcovschi"
+                      >
+                        m.me/cristi.satcovschi
+                      </a>
+                    </div>
+                  </li>
+                  <li className="flex flex-row space-x-8">
+                    <img
+                      width="64"
+                      height="64"
+                      src="https://img.icons8.com/stickers/100/instagram-new--v1.png"
+                      alt="instagram-new--v1"
+                    />
+                    <div className="flex flex-grow items-center">
+                      <a
+                        className="text-blue-600 dark:text-blue-500 break-all font-medium underline hover:no-underline"
+                        href="https://www.instagram.com/cristi.satcovschi"
+                      >
+                        instagram.com/cristi.satcovschi
+                      </a>
+                    </div>
+                  </li>
+                  <li className="flex flex-row space-x-8">
+                    <img
+                      width="64"
+                      height="64"
+                      src="https://img.icons8.com/stickers/100/github.png"
+                      alt="github"
+                    />
+                    <div className="flex flex-grow items-center">
+                      <a
+                        className="text-blue-600 dark:text-blue-500 break-all font-medium underline hover:no-underline"
+                        href="https://github.com/cristi-satcovschi"
+                      >
+                        github.com/cristi-satcovschi
+                      </a>
+                    </div>
+                  </li>
+                </ul>
+              </InfoCard>
             </div>
           </div>
 
@@ -146,6 +246,34 @@ const Home: NextPage = () => {
 
                 <FeedTags />
               </div>
+              <FeedCard>
+                <img
+                  className="float-right rotate-12"
+                  width="64"
+                  height="64"
+                  src="https://img.icons8.com/stickers/100/new.png"
+                  alt="new"
+                />
+                <h2 className="font-semibold">Post title</h2>
+                <br />
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Consequuntur maiores voluptas beatae, repellendus sint, ut
+                officia animi ratione alias dolor, libero excepturi! Reiciendis
+                velit magni nulla corrupti enim dolor necessitatibus.
+                <br />
+                <br />
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                Placeat possimus, distinctio explicabo, harum incidunt hic
+                dolore sequi aspernatur quibusdam non eveniet, recusandae illum
+                omnis at sit consectetur commodi culpa autem. Lorem ipsum dolor,
+                sit amet consectetur adipisicing elit. Placeat possimus,
+                distinctio explicabo, harum incidunt hic dolore sequi aspernatur
+                quibusdam non eveniet, recusandae illum omnis at sit consectetur
+                commodi culpa autem. Lorem ipsum dolor, sit amet consectetur
+                adipisicing elit. Placeat possimus, distinctio explicabo, harum
+                incidunt hic dolore sequi aspernatur quibusdam non eveniet,
+                recusandae illum omnis at sit consectetur commodi culpa autem.
+              </FeedCard>
 
               {[...Array(5).keys()].map((feedItem) => (
                 <FeedCard key={feedItem}>
