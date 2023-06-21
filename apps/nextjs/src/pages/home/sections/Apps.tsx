08ab772b9f2a6ed11c34eb17b47d5e76ff312a4b
cristi <cristisatcovschi123@gmail.com>
2023-06-21 18:18:40 +0300

    app list: add scroll on overflow for desktop version

--- apps/nextjs/src/pages/home/sections/Apps.tsx
+++ apps/nextjs/src/pages/home/sections/Apps.tsx
@@ -2,7 +2,7 @@ import AppCard from "../components/AppCard";
 import apps from "../data/apps.json";
 
 const Apps = () => (
-  <div className="bg-slate-50 z-10 flex flex-row space-x-8 overflow-x-scroll border-b p-16 shadow lg:justify-center">
+  <div className="bg-slate-50 z-10 flex flex-row space-x-8 overflow-x-scroll border-b p-16 shadow lg:justify-center lg:overflow-x-auto">
     {apps.map((app) => (
       <AppCard app={app} key={app.appName} />
     ))}
